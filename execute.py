d50f9034c6aa800ee15a65e03aa9b53cd73896fa
sokumon <sohamkulkarns9@gmail.com>
2022-06-02 17:46:03 +0530

    prototypinggoesonnnn

---
+++ execute.py
@@ -0,0 +1,11 @@
+
+# Importing required module
+import os
+  
+# Using system() method to execute
+# shell commands
+cmd="gcc trial.c"
+result=os.popen(cmd)
+cmd2="a.exe"
+result2=os.popen(cmd2)
+print(result2.read())
---
+++ trial.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a=0;
+    printf("Hello Sohame is here");
+    scanf("%d",&a);
+    printf("the number is %d ",a);
+    return 0;
+}
