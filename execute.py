c9a3f6891866a955fd72fdafd86a0376ab1a1c76
sokumon <sohamkulkarns9@gmail.com>
2022-06-02 17:54:53 +0530

    prototype2

--- execute.py
+++ execute.py
@@ -6,6 +6,6 @@ import os
 # shell commands
 cmd="gcc trial.c"
 result=os.popen(cmd)
-cmd2="a.exe"
+cmd2="./a.out"
 result2=os.popen(cmd2)
 print(result2.read())
--- trial.c
+++ trial.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
-    int a=0;
+    int a=0,b;
     printf("Hello Sohame is here");
-    scanf("%d",&a);
-    printf("the number is %d ",a);
+    printf("Enter a Number");
+    scanf("%d",&b);
+    printf("the number is %d ",b);
     return 0;
 }
